431d1b52c8d72ec841d1e6f2578ad53809df68bf
Arthur <arthurchiuchiu@outlook.com>
2024-09-28 00:41:24 +0100

    project solution files update

---
+++ _Compile_Release.bat
@@ -0,0 +1,4 @@
+rem msbuild.exe Smtm_2005.sln /p:Configuration=Release /t:Rebuild
+rem msbuild.exe Smtm_2005.sln /p:Configuration=Release /t:Clean
+msbuild.exe Smtm_2005.sln /p:Configuration=Release
+pause
