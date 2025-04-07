db99811050232e2ec7a5b861406caf3099f1bd2b
Arthur <arthurchiuchiu@outlook.com>
2025-04-07 16:01:37 +0100

    comment translation from korean to english

--- ItemEdit/ItemEdit.cpp
+++ ItemEdit/ItemEdit.cpp
@@ -1,4 +1,4 @@
-// ItemEdit.cpp : 응용 프로그램에 대한 클래스 동작을 정의합니다.
+// ItemEdit.cpp : Defines class behavior for an application..
 //
 
 #include "stdafx.h"
@@ -23,12 +23,12 @@ BEGIN_MESSAGE_MAP(CItemEditApp, CWinApp)
 END_MESSAGE_MAP()
 
 
-// CItemEditApp 생성
+// CItemEditApp generation
 
 CItemEditApp::CItemEditApp()
 {
-	// TODO: 여기에 생성 코드를 추가합니다.
-	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+	// TODO: Add the generation code here.
+	// InitInstance Place all important initialization tasks in .
 	sc::initialize();
     GLogicData::GetInstance().LoadCountryFile();
     m_emServiceProvider = GLogicData::GetInstance().GetServiceProvider();
@@ -45,16 +45,16 @@ CItemEditApp::~CItemEditApp()
 	sc::finalize();
 }
 
-// 유일한 CItemEditApp 개체입니다.
+// only CItemEditApp It's an object.
 CItemEditApp theApp;
 
-// CItemEditApp 초기화
+// CItemEditApp reset
 
 BOOL CItemEditApp::InitInstance()
 {
-	// 응용 프로그램 매니페스트가 ComCtl32.dll 버전 6 이상을 사용하여 비주얼 스타일을
-	// 사용하도록 지정하는 경우, Windows XP 상에서 반드시 InitCommonControls()가 필요합니다. 
-	// InitCommonControls()를 사용하지 않으면 창을 만들 수 없습니다.
+	// The application manifest uses ComCtl32.dll version 6 or later to provide visual styles.
+	// If you specify to use it, InitCommonControls() is absolutely required on Windows XP.
+	// You cannot create a window without using InitCommonControls().
 	InitCommonControls();
 
 	CWinApp::InitInstance();
@@ -68,16 +68,16 @@ BOOL CItemEditApp::InitInstance()
 	INT_PTR nResponse = dlg.DoModal();
 	if (nResponse == IDOK)
 	{
-		// TODO: 여기에 대화 상자가 확인을 눌러 없어지는 경우 처리할
-		// 코드를 배치합니다.
+		// TODO: Here is what to do if the dialog box disappears when you click OK
+		// Deploy the code.
 	}
 	else if (nResponse == IDCANCEL)
 	{
-		// TODO: 여기에 대화 상자가 취소를 눌러 없어지는 경우 처리할
-		// 코드를 배치합니다.
+		// TODO: Here is what to do if the dialog box disappears when you press cancel.
+		// Deploy the code.
 	}
 
-	// 대화 상자가 닫혔으므로 응용 프로그램의 메시지 펌프를 시작하지 않고
-	// 응용 프로그램을 끝낼 수 있도록 FALSE를 반환합니다.
+	// The dialog box is closed, so the application's message pump is not started.
+	// Returns FALSE to allow the application to exit.
 	return FALSE;
 }
